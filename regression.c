8e1a19a37060488203752e6c864b8379a282fb50
Sadia Tabassum <60074200+SadiaTabassum1216@users.noreply.github.com>
2022-03-24 00:16:40 +0600

    Update regression.c
    Matrix: co-factor calculation, inverse matrix

--- regression.c
+++ regression.c
@@ -4,7 +4,10 @@
 
 float determinant(int row, float matrix[row][row]);
 void transpose(int row,int column,float matrix1[row][column],float matrix2[column][row]);
+void coFactor(int row, float matrix[row][row],float output[row][row]);
 void multiplication(int row1,int column1,int row2,int column2,float matrix1[row1][column1],float matrix2[row2][column2],float mul[row1][column2]);
+void inverse(int row, float input[row][row],float inverseMatrix[row][row]);
+
 
 int main()
 {
@@ -13,6 +16,7 @@ printf("Enter the row and column number:");
 scanf("%d %d",&row,&column);
 
 float m1[row][column],m2[column][row],mul[row][row],det;
+float inverseMatrix[row][row];
 
 printf("Enter the matrix:\n");
 for(i=0;i<row;i++){
@@ -21,12 +25,23 @@ for(i=0;i<row;i++){
     }
 }
 
-    transpose(row,column,m1,m2);
-    multiplication(row,column,column,row,m1,m2,mul);
-
-
-    det= determinant(row,mul);
+//    transpose(row,column,m1,m2);
+//    multiplication(row,column,column,row,m1,m2,mul);
+    det= determinant(row,m1);
     printf("\nDeterminant= %.2f",det);
+
+inverse(row,m1,inverseMatrix);
+
+
+printf("\nInverse Matrix:\n");
+    for(i=0;i<row;i++){
+        for(j=0;j<row;j++){
+            printf("%.4f ",inverseMatrix[i][j]);
+        }
+        printf("\n");
+    }
+
+
 return 0;
 }
 
@@ -40,23 +55,25 @@ void transpose(int row,int column,float m1[row][column],float m2[column][row])
             m2[j][i]=m1[i][j];
         }
     }
-printf("\nTranspose Matrix:\n");
-    for(i=0;i<column;i++){
-        for(j=0;j<row;j++){
-            printf("%.2f ",m2[i][j]);
-        }
-        printf("\n");
-    }
+//printf("\nTranspose Matrix:\n");
+//    for(i=0;i<column;i++){
+//        for(j=0;j<row;j++){
+//            printf("%.2f ",m2[i][j]);
+//        }
+//        printf("\n");
+//    }
 }
 
 
 float determinant(int row, float matrix[row][row])
 {
     int i,j,k,m,n,sign=1;
+
+    float det=0, b[row][row];
     if(row==1)
         return matrix[0][0];
 
-    float det=0, b[row][row];
+    det=0;
     for(k=0;k<row;k++){
             m=0;
             n=0;
@@ -65,19 +82,94 @@ float determinant(int row, float matrix[row][row])
                 if(i!=0 && j!=k){
                     b[m][n]=matrix[i][j];
                     n++;
-                    if(n==row-1)
+                    if(n>=row-2)
+                    {
+                        n=0;
+                        m++;
+                    }
+//                if (n<(row-2))
+//                    n++;
+//                   else
+//                    {
+//                     n=0;
+//                     m++;
+//                     }
+                }
+            }
+        }
+        det=det+sign*(matrix[0][k]*determinant(row-1,b));
+        //printf("\n** %.2lf",det);
+        sign=-1*sign;
+    }
+return det;
+}
+
+
+void coFactor(int row, float matrix[row][row],float output[row][row])
+{
+    int i,j,k,l,m,n,sign=1;
+for(l=0;l<row;l++){
+    for(k=0;k<row;k++){
+            m=0;
+            n=0;
+        for(i=0;i<row;i++){
+            for(j=0;j<row;j++){
+                if(i!=l && j!=k){
+                    output[m][n]=matrix[i][j];
+                    n++;
+                    if(n==row-2)
                     {
                         n=0;
                         m++;
                     }
                 }
+
             }
         }
-        det=det+sign*(matrix[0][k]*determinant(row-1,b));
-        sign=sign*(-1);
     }
-return det;
 }
+}
+
+
+void inverse(int row, float input[row][row],float inverseMatrix[row][row]){
+
+float cofactors[row][row],transposeMatrix[row][row];
+int i,j;
+coFactor(row,input,cofactors);
+
+printf("\nCofactor:\n");
+    for(i=0;i<row;i++){
+        for(j=0;j<row;j++){
+            printf("%.4f ",cofactors[i][j]);
+        }
+        printf("\n");
+    }
+
+transpose(row,row,cofactors,transposeMatrix);
+
+printf("\nTranspose Matrix:\n");
+    for(i=0;i<row;i++){
+        for(j=0;j<row;j++){
+            printf("%.4f ",transposeMatrix[i][j]);
+        }
+        printf("\n");
+    }
+
+float d=determinant(row,input);
+
+
+for(i=0;i<row;i++){
+    for(j=0;j<row;j++){
+        inverseMatrix[i][j]=transposeMatrix[i][j]/d;
+    }
+}
+
+}
+
+
+
+
+
 
 void multiplication(int row1,int column1,int row2,int column2,float matrix1[row1][column1],float matrix2[row2][column2],float mul[row1][column2])
 {
